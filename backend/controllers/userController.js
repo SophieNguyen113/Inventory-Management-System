fb6fd59d8493b3b37beff2a08e0c803f52f1cb44
Sophie Nguyen <sophiengocnguyen113@gmail.com>
2023-06-23 14:41:16 -0400

    Intial Commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,13 +37,16 @@ const registerUser = asyncHandler (async (req, res) => {
 
     const token = generateToken(user._id);
 
-    res.cookie("token", token, {
-        path: "/",
-        httpOnly: true,
-        exprires: new Date(Date.now() + 1000 * 86400),
-        sameSite: "none",
-        secure: true
-    })
+    if (passwordIsCorrect) {
+        res.cookie("token", token, {
+            path: "/",
+            httpOnly: true,
+            exprires: new Date(Date.now() + 1000 * 86400),
+            sameSite: "none",
+            secure: true
+        })
+    }
+
 
     if (user) {
         const { _id, name, email, photo, phone, bio } = user;
@@ -98,7 +101,81 @@ const loginUser = asyncHandler( async (req, res) => {
 
 });
 
+const logoutUser = asyncHandler (async (req, res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        exprires: new Date(0),
+        sameSite: "none",
+        secure: true
+    })
+    return res.status(200).json({message: "Successfully Logged Out"})
+})
+
+const getUser = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(200).json({
+            _id, name, email, photo, phone, bio, 
+        });     
+    } else {
+        res.status(400);
+        throw new Error('User Not Found');   
+    }
+})
+
+const loginStatus = asyncHandler (async (req, res) => {
+    const token = req.cookies.token
+        if(!token){
+            return res.json(false)
+        }
+    
+    const verified = jwt.verify(token, process.env.JWT_SECRET)
+    if(verified){
+        return res.json(true)
+    }
+    return res.json(false)
+})
+
+const updateUser = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        const { name, email, photo, phone, bio } = user;
+        user.email = email
+        user.name = req.body.name || name
+        user.phone = req.body.phone || phone
+        user.bio = req.body.bio || bio
+        user.photo = req.body.photo || photo
+
+        const updatedUser = await user.save()
+    res.status(200).json({
+        _id: updatedUser._id, 
+        name: updatedUser.name, 
+        email: updatedUser.email, 
+        photo: updatedUser.photo, 
+        phone: updatedUser.phone, 
+        bio: updatedUser.bio,
+    })
+    } else {
+        res.status(404);
+        throw new Error('User not found');  
+    }
+
+})
+
+const changePassword = asyncHandler (async (req, res) => {
+    res.send('Sccuesslly HCnage dPasswordc')
+})
+
 module.exports = {
     registerUser,
     loginUser,
+    logoutUser,
+    getUser,
+    loginStatus,
+    updateUser,
+    changePassword
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();  
-const { registerUser, loginUser } = require('../controllers/userController');  
+const { registerUser, loginUser, logoutUser, getUser, loginStatus, updateUser, changePassword } = require('../controllers/userController');  
+const protect = require('../middleWare/authMiddleware');  
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/logout', logoutUser);
+router.get('/getuser', protect, getUser);
+router.get('/loggedin', loginStatus);
+router.patch('/updateuser', protect, updateUser);
+router.patch('/changepassword', protect, changePassword);
+
+
 
 module.exports = router;    
